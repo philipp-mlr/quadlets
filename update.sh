31457a01b7f619bf548117035af136ad472d641b
Philipp Mueller <hello@philipp.software>
2025-03-20 20:48:38 +0100

    feat: add fancy heading

--- update.sh
+++ update.sh
@@ -102,6 +102,18 @@ update_config() {
   start_services "$config_dir"
 }
 
+script_hello="""
+   ____                 _ _      _                     _       _       
+  /___ \_   _  __ _  __| | | ___| |_   /\ /\ _ __   __| | __ _| |_ ___ 
+ //  / / | | |/ _` |/ _` | |/ _ \ __| / / \ \ '_ \ / _` |/ _` | __/ _ \
+/ \_/ /| |_| | (_| | (_| | |  __/ |_  \ \_/ / |_) | (_| | (_| | ||  __/
+\___,_\ \__,_|\__,_|\__,_|_|\___|\__|  \___/| .__/ \__,_|\__,_|\__\___|
+                                            |_|                        
+                                             by @github.com/philipp-mlr
+"""
+
+echo -e "$script_hello"
+
 # 🚀 Main script execution
 update_config ~/.config/containers/systemd/rootless
 update_config /etc/containers/systemd/rootful
