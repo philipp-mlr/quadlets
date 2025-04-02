ac2fe8db2442c16ca1731126451019c6d21006b4
Philipp Mueller <hello@philipp.software>
2025-04-02 09:44:42 +0200

    fix: special chars in env names

--- update.sh
+++ update.sh
@@ -4,7 +4,8 @@ start_services() {
   local service_dir="$1"
 
   find "$service_dir" -name "*.container" -print0 | while IFS= read -r -d $'\0' service_file; do
-    local service_name=$(basename "$service_file" .container)
+    local service_name
+    service_name=$(basename "$service_file" .container)
     local status
 
     if [[ "$service_dir" == "/etc/containers/systemd/rootful" ]]; then
@@ -14,7 +15,7 @@ start_services() {
     fi
 
     if [[ "$status" != "active" ]]; then
-      echo -e "\n  ▶️ Starting service: $service_name"
+      echo -e "\n  ▶️ Starting service: $service_name"
 
       if [[ "$service_dir" == "/etc/containers/systemd/rootful" ]]; then
         sudo systemctl start "$service_name"
@@ -23,9 +24,9 @@ start_services() {
       fi
 
       if [[ $? -eq 0 ]]; then
-        echo -e "  ✅ Service $service_name started successfully.\n"
+        echo -e "  ✅ Service $service_name started successfully.\n"
       else
-        echo -e "  ❌ Failed to start service $service_name.\n"
+        echo -e "  ❌ Failed to start service $service_name.\n"
       fi
     fi
   done
@@ -47,37 +48,39 @@ update_config() {
 
   echo -e "\n🔄 Updating systemd $(basename "$config_dir") configuration..."
 
-  echo -e "\n  🗑️ Removing old configuration..."
+  echo -e "\n  🗑️ Removing old configuration..."
   ${sudo_prefix} rm -rf "$config_dir" || true
 
   if [[ "$is_rootful" == true ]]; then
-    echo -e "\n  📂 Creating new rootful configuration directory..."
+    echo -e "\n  📂 Creating new rootful configuration directory..."
     ${sudo_prefix} mkdir -p "$config_dir"
   fi
 
-  echo -e "\n  📂 Copying new configuration..."
+  echo -e "\n  📂 Copying new configuration..."
   ${sudo_prefix} cp -r "$source_dir/." "$config_dir/"
 
   if [[ $? -eq 0 ]]; then
-    echo -e "  ✅ Configuration copied successfully.\n"
+    echo -e "  ✅ Configuration copied successfully.\n"
   else
-    echo -e "  ❌ Failed to copy configuration!\n"
+    echo -e "  ❌ Failed to copy configuration!\n"
   fi
 
-  echo -e "\n  🔃 Load global environment file"
+  echo -e "\n  🔃 Load global environment file"
   if [[ -f "./.env" ]]; then
-    while IFS= read -r line; do
+    while IFS= read -r line || [[ -n "$line" ]]; do
       if [[ "$line" =~ ^([^#=]+)=(.*)$ ]]; then
-        export "${BASH_REMATCH[1]}=${BASH_REMATCH[2]}"
-        env_vars+=("${BASH_REMATCH[1]}=${BASH_REMATCH[2]}")
+        key="${BASH_REMATCH[1]}"
+        value="${BASH_REMATCH[2]}"
+        export "$key"="$value"
+        env_vars+=("$key=$value")
       fi
     done < "./.env"
   fi
 
-  echo -e "\n  🔄 Checking for empty environment variables..."
+  echo -e "\n  🔄 Checking for empty environment variables..."
   ${sudo_prefix} find "$config_dir" -name "*.env" -exec bash -c '
     for env_file in "$@"; do
-      while IFS= read -r line; do
+      while IFS= read -r line || [[ -n "$line" ]]; do
         if [[ "$line" =~ ^([^#=]+)=[[:space:]]*$ ]]; then
           echo "❌ Error: Empty environment variable detected in $env_file: $line"
           exit 1
@@ -87,28 +90,30 @@ update_config() {
   ' bash {} +
 
   if [[ $? -ne 0 ]]; then
-    echo -e "  ❌ Exiting due to empty environment variables.\n"
+    echo -e "  ❌ Exiting due to empty environment variables.\n"
     exit 1
   fi
 
-  echo -e "\n  🔄 Replacing environment variables in container files"
+  echo -e "\n  🔄 Replacing environment variables in container files"
   ${sudo_prefix} find "$config_dir" -name "*.container" -exec bash -c '
     for container_file in "$@"; do
       env_file="${container_file%.container}.env"
       if [[ -f "$env_file" ]]; then
-        source "$env_file"
+        set -a  # Export all variables automatically
+        . "$env_file"
+        set +a  # Stop automatic export
       fi
-      envsubst < "$container_file" > "$container_file.new" && mv "$container_file.new" "$container_file"
+      export | envsubst < "$container_file" > "$container_file.new" && mv "$container_file.new" "$container_file"
     done
   ' bash {} +
 
-  echo -e "\n  🔄 Reloading systemd..."
+  echo -e "\n  🔄 Reloading systemd..."
   $systemctl_reload
 
   if [[ $? -eq 0 ]]; then
-    echo -e "  ✅ Systemd reloaded successfully.\n"
+    echo -e "  ✅ Systemd reloaded successfully.\n"
   else
-    echo -e "  ❌ Failed to reload systemd!\n"
+    echo -e "  ❌ Failed to reload systemd!\n"
   fi
 
   echo -e "✅ $(basename "$config_dir") configuration updated.\n"
